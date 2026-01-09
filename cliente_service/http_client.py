71b4df9fe2d664f80db4f58ce8ad10eff2f1d1d5
RafaelCespedes-br <rafael.cespedes@ibm.com>
2026-01-09 11:46:13 -0300

    Ending connect

--- cliente_service/http_client.py
+++ cliente_service/http_client.py
@@ -10,3 +10,26 @@ def criar_cliente_no_database_service(cliente: dict):
     )
     response.raise_for_status()
     return response.json()
+
+def listar_clientes_do_database_service():
+    response = requests.get(
+        DATABASE_SERVICE_URL
+    )
+
+    response.raise_for_status()
+    return response.json()
+
+def atualizar_cliente_no_database_service(cliente_id: int, cliente: dict):
+    response = requests.put(
+        f"{DATABASE_SERVICE_URL}/{cliente_id}",
+        json=cliente
+    )
+    response.raise_for_status()
+    return response.json()
+
+def deletar_cliente_no_database_service(cliente_id: int):   
+    response = requests.delete(
+        f"{DATABASE_SERVICE_URL}/{cliente_id}"
+    )
+    response.raise_for_status()
+    return response.json()
--- cliente_service/main.py
+++ cliente_service/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from cliente_service.models import ClienteEntrada, ClienteCompleto
 from cliente_service.service import calcular_score
-from cliente_service.http_client import criar_cliente_no_database_service
+from cliente_service.http_client import criar_cliente_no_database_service, listar_clientes_do_database_service, atualizar_cliente_no_database_service, deletar_cliente_no_database_service
 
 app = FastAPI()
 
@@ -27,4 +27,39 @@ def criar_cliente(cliente: ClienteEntrada):
 
     resposta = criar_cliente_no_database_service(cliente_completo.dict())
 
+    return resposta
+
+
+
+@app.get("/clientes")
+def listar_clientes():
+
+    clientes = listar_clientes_do_database_service()
+    return clientes
+
+
+@app.put("/clientes/{cliente_id}")
+def atualizar_cliente(cliente_id: int, cliente: ClienteEntrada):
+
+    score = calcular_score(
+        saldo_cc=cliente.saldo_cc,
+        correntista=cliente.correntista
+    )
+
+    cliente_completo = ClienteCompleto(
+        nome=cliente.nome,
+        telefone=cliente.telefone,
+        saldo_cc=cliente.saldo_cc,
+        correntista=cliente.correntista,
+        score_credito=score
+    )
+
+    resposta = atualizar_cliente_no_database_service(cliente_id, cliente_completo.dict())
+
+    return resposta
+
+@app.delete("/clientes/{cliente_id}")
+def deletar_cliente(cliente_id: int):
+       
+    resposta = deletar_cliente_no_database_service(cliente_id)
     return resposta
